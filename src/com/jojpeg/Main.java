87bb01c99bfa9f4a434063f5ee981f2600c57b06
JoJpeg <jonas.eschner@gmx.de>
2017-07-22 12:52:42 +0200

    Added Main (?)

---
+++ src/com/jojpeg/Main.java
@@ -0,0 +1,11 @@
+package com.jojpeg;
+
+import processing.core.PApplet;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        PApplet.main("com.jojpeg.ProcessingCore");
+    }
+}
